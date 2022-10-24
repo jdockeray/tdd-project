3e74d555948e4ca0d548f8a8b8feaae7033549d5
James dockeray <jdockeray20@gmail.com>
2022-10-24 12:45:14 +0100

    feat: go tests

--- go/money/src/money_test.go
+++ go/money/src/money_test.go
@@ -2,24 +2,64 @@ package main
 
 import "testing"
 
-type Dollar struct {
-	amount int
+type Money struct {
+	amount   float64
+	currency string
 }
 
-func (d Dollar) Times(multiplier int) Dollar {
-	return Dollar{
-		amount: d.amount * multiplier,
+func (d Money) Times(multiplier int) Money {
+	return Money{
+		amount:   d.amount * float64(multiplier),
+		currency: d.currency,
+	}
+}
+
+func (d Money) Divide(divisor int) Money {
+	return Money{
+		amount:   d.amount / float64(divisor),
+		currency: d.currency,
+	}
+}
+
+func assertEquals(t *testing.T, expected Money, actual Money) {
+	if expected != actual {
+		t.Errorf("Expected %+v Got %+v", expected, actual)
 	}
 }
 
 func TestMultiplication(t *testing.T) {
-	fiver := Dollar{
-		amount: 5,
+	fiver := Money{
+		amount:   5,
+		currency: "USD",
 	}
 
-	tenner := fiver.Times(2)
-
-	if tenner.amount != 10 {
-		t.Errorf("Expected 10 got %d", tenner.amount)
+	actualMoneyBeforeMultiplication := fiver.Times(2)
+	expectedMoneyAfterMultiplication := Money{
+		amount:   10,
+		currency: "USD",
 	}
+	assertEquals(t, actualMoneyBeforeMultiplication, expectedMoneyAfterMultiplication)
+}
+
+func TestMultiplicationInEuros(t *testing.T) {
+	tenEuros := Money{
+		amount:   10,
+		currency: "EUR",
+	}
+
+	actualMoneyAfterMultiplication := tenEuros.Times(2)
+	expectedMoneyAfterMultiplication := Money{
+		amount:   20,
+		currency: "EUR",
+	}
+
+	assertEquals(t, actualMoneyAfterMultiplication, expectedMoneyAfterMultiplication)
+}
+
+func TestDivision(t *testing.T) {
+	originalMoney := Money{amount: 4002, currency: "KRW"}
+	actualMoneyAfterDivision := originalMoney.Divide(4)
+	expectedMoneyAfterDivision := Money{amount: 1000.5, currency: "KRW"}
+
+	assertEquals(t, actualMoneyAfterDivision, expectedMoneyAfterDivision)
 }
